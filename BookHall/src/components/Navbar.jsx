638f66ea51651dacc1b08729a01c7f65b0684696
JagratiSharma16 <j.sharma1416@gmail.com>
2025-04-08 15:04:18 +0530

    Navbar updated

--- BookHall/src/components/Navbar.jsx
+++ BookHall/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
                 <div> 
                     <div className="flex items-center justify-between text-5m py-5 mb-5 border-b-3 border-b-gray-400 ml-50 mr-50  shadow-lg">
                         <img className="w-26 rounded-2xl cursor-pointer" src={assets.Booklogo} alt="image" />
-                        <ul className="hidden md:flex items-start gap-10 font-bold text-2xl">
+                        <ul className="hidden md:flex items-start gap-10 font-bold xl:text-2xl 2xl:text-4xl">
                             <NavLink to="/">
                                 <li className="py-1">Home</li>
                                 <hr className="border-none outline-none h-1 bg-blue-500 w-5/5 m-auto hidden" />
@@ -18,12 +18,8 @@ const Navbar = () => {
                                 <li className="py-1">Admin</li>
                                 <hr className="border-none outline-none h-1 bg-blue-500 w-5/5 m-auto hidden" />
                             </NavLink>
-                            <NavLink to="/society">
-                                <li className="py-1">Society</li>
-                                <hr className="border-none outline-none h-1 bg-blue-500 w-5/5 m-auto hidden" />
-                            </NavLink>
-                            <NavLink to="/Departmemt">
-                                <li className="py-1">Department</li>
+                            <NavLink to="/about">
+                                <li className="py-1">About</li>
                                 <hr className="border-none outline-none h-1 bg-blue-500 w-5/5 m-auto hidden" />
                             </NavLink>
                         </ul>
